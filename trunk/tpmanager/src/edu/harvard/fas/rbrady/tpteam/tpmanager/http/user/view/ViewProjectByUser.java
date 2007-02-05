c8ebb7f6012cf46ca2dd5faf31cb8d6a7abcd1b7
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-02-05 19:49:22 +0000

    Initial Commit.

---
+++ trunk/tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/http/user/view/ViewProjectByUser.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Robert Brady. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Robert Brady - initial API and implementation
+ ******************************************************************************/
+
+package edu.harvard.fas.rbrady.tpteam.tpmanager.http.user.view;
+
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import edu.harvard.fas.rbrady.tpteam.tpmanager.Activator;
+import edu.harvard.fas.rbrady.tpteam.tpmanager.hibernate.Project;
+import edu.harvard.fas.rbrady.tpteam.tpmanager.hibernate.TpteamUser;
+import edu.harvard.fas.rbrady.tpteam.tpmanager.http.ServletUtil;
+import edu.harvard.fas.rbrady.tpteam.tpmanager.http.UserServlet;
+import edu.harvard.fas.rbrady.tpteam.tpmanager.http.admin.view.ViewProject;
+
+public class ViewProjectByUser extends ViewProject implements UserServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected String getProjRows() throws Exception
+{
+		Transaction tx = null;
+		Set<Project> projs = null;
+		StringBuffer projRows = new StringBuffer();
+		try {
+			int remoteUserId = ServletUtil.getRemoteUserID(mRemoteUser);
+			// For standalone
+			//Session s = HibernateUtil.getSessionFactory().getCurrentSession();
+			Session s = Activator.getDefault().getHiberSessionFactory()
+					.getCurrentSession();
+			tx = s.beginTransaction();
+			TpteamUser user = (TpteamUser) s.createQuery(
+					"from TpteamUser as user where user.id = " + remoteUserId)
+					.uniqueResult();
+			projs = user.getProjects();
+
+			for(Project proj : projs)
+			{
+				projRows.append(rowNameHeader + proj.getName());
+				if(proj.getDescription() != null && !proj.getDescription().equalsIgnoreCase("null"))
+				{
+					projRows.append(": " + proj.getDescription());
+				}
+				projRows.append("</td>\n" + rowIDHeader + " value=\"" + proj.getId() + "\">\n" + rowSubmitHeader + "\n");
+			}
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		}
+		if(projRows.length() > 0)
+			mIsProjAvailable = true;
+		
+		mProjRows = projRows.toString();
+		
+		return mProjRows;
+	}
+}

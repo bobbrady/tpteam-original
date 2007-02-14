21251426226e4cfdb3302ddc00ec254b06f7c304
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-02-14 03:54:28 +0000

    Updated perspective to show TestTree, EventHistory, and RosterView.

--- trunk/tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/Perspective.java
+++ trunk/tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/Perspective.java
@@ -8,16 +8,40 @@
  ******************************************************************************/
 package edu.harvard.fas.rbrady.tpteam.tpbuddy;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+
+import edu.harvard.fas.rbrady.tpteam.tpbuddy.views.EventHistoryView;
+import edu.harvard.fas.rbrady.tpteam.tpbuddy.views.TestView;
 
 
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
 		layout.setEditorAreaVisible(false);
-		//layout.addView(TestView.ID, IPageLayout.LEFT, .5f, layout.getEditorArea());
-		//layout.addView(TreeTestView.ID, IPageLayout.LEFT, 1.0f, layout.getEditorArea());
+
+        //layout.createPlaceholderFolder(IPageLayout.ID_EDITOR_AREA, IPageLayout.LEFT, 1.00f, layout.getEditorArea());
+		 IFolderLayout topFolder = layout.createFolder("top", IPageLayout.TOP, 0.65f, layout.getEditorArea());
+		 topFolder.addView(TestView.ID);
+		 topFolder.addView(TestView.ID + ":2" );
+		//layout.addView(TestView.ID, IPageLayout.TOP, 0.65f, layout.getEditorArea());
+		layout.addView("org.eclipse.ecf.ui.view.rosterview", IPageLayout.RIGHT, 0.7f, "top");
+		layout.addView(EventHistoryView.ID, IPageLayout.BOTTOM, 0.35f, layout.getEditorArea());
 		//layout.addStandaloneView(TestView.ID, false,IPageLayout.LEFT, 1.0f, layout.getEditorArea());
+
+		/*
+        layout.createPlaceholderFolder(EDITOR_AREA, IPageLayout.LEFT, 1.00f, IPageLayout.ID_EDITOR_AREA);
+        layout.createPlaceholderFolder("left", IPageLayout.LEFT, 0.20f, EDITOR_AREA);
+        layout.createPlaceholderFolder("bottom", IPageLayout.BOTTOM, 0.80f, EDITOR_AREA);
+        layout.createPlaceholderFolder("right", IPageLayout.RIGHT, 0.70f, EDITOR_AREA);
+        */
+		/*
+		 IFolderLayout topFolder = layout.createFolder("top", IPageLayout.TOP, 0.7f, layout.getEditorArea());
+		 topFolder.addView(TestView.ID);
+		 topFolder.addView("org.eclipse.ecf.ui.view.rosterview");
+		 layout.addView(EventHistoryView.ID, IPageLayout.BOTTOM, 0.3f, layout.getEditorArea());
+		 */
+
 	}
 }

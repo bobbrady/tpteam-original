5a65ff63c645986650bcf869b3ebcbe683f1d96d
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-02-27 20:43:23 +0000

    Modified to support loading TPBridge Model TPEntity tree objects into Test View of TPBuddy.

--- tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/views/TestContentProvider.java
+++ tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/views/TestContentProvider.java
@@ -8,43 +8,23 @@
  ******************************************************************************/
 package edu.harvard.fas.rbrady.tpteam.tpbuddy.views;
 
-import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.jface.viewers.Viewer;
 
-import edu.harvard.fas.rbrady.tpteam.tpbuddy.model.TreeObject;
-import edu.harvard.fas.rbrady.tpteam.tpbuddy.model.TreeParent;
+import edu.harvard.fas.rbrady.tpteam.tpbridge.model.TPEntity;
 
-public class TestContentProvider implements IStructuredContentProvider,
-ITreeContentProvider {
+public class TestContentProvider extends ArrayContentProvider implements
+		ITreeContentProvider {
 
-public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-}
+	public Object getParent(Object child) {
+		return ((TPEntity) child).getParent();
+	}
 
-public void dispose() {
-}
+	public Object[] getChildren(Object parent) {
+		return ((TPEntity) parent).getChildren();
+	}
 
-public Object[] getElements(Object parent) {
-return getChildren(parent);
-}
-
-public Object getParent(Object child) {
-if (child instanceof TreeObject) {
-    return ((TreeObject) child).getParent();
-}
-return null;
-}
-
-public Object[] getChildren(Object parent) {
-if (parent instanceof TreeParent) {
-    return ((TreeParent) parent).getChildren();
-}
-return new Object[0];
-}
-
-public boolean hasChildren(Object parent) {
-if (parent instanceof TreeParent)
-    return ((TreeParent) parent).hasChildren();
-return false;
-}
+	public boolean hasChildren(Object parent) {
+		return ((TPEntity)parent).getChildren().length > 0;
+	}
 }
--- tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/views/TestView.java
+++ tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/views/TestView.java
@@ -9,7 +9,6 @@
 package edu.harvard.fas.rbrady.tpteam.tpbuddy.views;
 
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Observable;
 import java.util.Observer;
@@ -24,14 +23,15 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.part.ViewPart;
+import org.w3c.dom.Document;
 
 import edu.harvard.fas.rbrady.tpteam.tpbridge.bridge.ITPBridge;
+import edu.harvard.fas.rbrady.tpteam.tpbridge.model.TPEntity;
 import edu.harvard.fas.rbrady.tpteam.tpbridge.model.TPEvent;
+import edu.harvard.fas.rbrady.tpteam.tpbridge.xml.TestXML;
+import edu.harvard.fas.rbrady.tpteam.tpbridge.xml.XMLUtil;
 import edu.harvard.fas.rbrady.tpteam.tpbuddy.Activator;
 import edu.harvard.fas.rbrady.tpteam.tpbuddy.eventadmin.EventAdminHandler;
-import edu.harvard.fas.rbrady.tpteam.tpbuddy.model.TPTestEntity;
-import edu.harvard.fas.rbrady.tpteam.tpbuddy.model.TreeObject;
-import edu.harvard.fas.rbrady.tpteam.tpbuddy.model.TreeParent;
 
 public class TestView extends ViewPart implements Observer {
 	public static final String ID = "edu.harvard.fas.rbrady.tpteam.tpbuddy.views.testview";
@@ -40,16 +40,17 @@ public class TestView extends ViewPart implements Observer {
 
 	private TreeViewer viewer;
 
-	private HashMap<String, TPTestEntity> mTPEntities;
+	private HashMap<String, TPEntity> mTPEntities;
 
 	private void updateAction() {
 		IStructuredSelection selection = (IStructuredSelection) viewer
 				.getSelection();
 		Iterator selectionIter = selection.iterator();
 		while (selectionIter.hasNext()) {
-			TreeObject treeObject = (TreeObject) selectionIter.next();
+			TPEntity treeEnt = (TPEntity) selectionIter.next();
 			System.out.println("\n\nTestView: Selection "
-					+ treeObject.getName());
+					+ treeEnt.getName());
+			/*
 			if (treeObject instanceof TPTestEntity) {
 				Hashtable<String, String> dictionary = ((TPTestEntity) treeObject)
 						.getDictionary();
@@ -57,6 +58,7 @@ public class TestView extends ViewPart implements Observer {
 						dictionary);
 				sendMsgToEventAdmin(tpEvent);
 			}
+			*/
 		}
 
 	}
@@ -81,63 +83,6 @@ public class TestView extends ViewPart implements Observer {
 		mgr.add(runTest);
 
 	}
-
-	/**
-	 * We will set up a dummy model to initialize tree heararchy. In real code,
-	 * you will connect to a real model and expose its hierarchy.
-	 */
-	private TreeObject createDummyModel() {
-		TreeParent pBuddy = new TreeParent("TPBuddy",
-				TreeObject.TEST_SUITE_TYPE, TreeObject.TEST_SUITE_IMAGE);
-		TreeParent pManager = new TreeParent("TPManager",
-				TreeObject.TEST_SUITE_TYPE, TreeObject.TEST_SUITE_IMAGE);
-		TreeParent pBridge = new TreeParent("TPBridge",
-				TreeObject.TEST_SUITE_TYPE, TreeObject.TEST_SUITE_IMAGE);
-		TreeParent pProj = new TreeParent("Demo Project (TPTeam)",
-				TreeObject.FOLDER_TYPE, TreeObject.FOLDER_IMAGE);
-		pProj.addChild(pBuddy);
-		pProj.addChild(pManager);
-		pProj.addChild(pBridge);
-
-		TreeParent pAdmin = new TreeParent("Admin", TreeObject.TEST_SUITE_TYPE,
-				TreeObject.TEST_SUITE_IMAGE);
-		TreeParent pUI = new TreeParent("UI", TreeObject.TEST_SUITE_TYPE,
-				TreeObject.TEST_SUITE_IMAGE);
-
-		pBuddy.addChild(pAdmin);
-		pBuddy.addChild(pUI);
-
-		TPTestEntity pAdmin1 = new TPTestEntity("TestEditTestSuite",
-				TreeObject.JUNIT_TYPE, TreeObject.JUNIT_IMAGE);
-		TPTestEntity pAdmin2 = new TPTestEntity("TestAddUser",
-				TreeObject.JUNIT_TYPE, TreeObject.JUNIT_IMAGE);
-
-		pAdmin.addChild(pAdmin1);
-		pAdmin.addChild(pAdmin2);
-
-		pAdmin1.setProject("Demo Project (TPTeam)");
-		pAdmin1.setDesc("Edit TestSuite feature check");
-		pAdmin1
-				.setStatus("Pass: 07/21/2006 14:07:32",
-						TreeObject.TEST_OK_IMAGE);
-		pAdmin1.setID("1");
-
-		pAdmin2.setProject("Demo Project (TPTeam)");
-		pAdmin2.setDesc("Add user feature check");
-		pAdmin2.setStatus("Fail: 07/21/2006 15:51:23",
-				TreeObject.TEST_FAIL_IMAGE);
-		pAdmin2.setID("2");
-
-		mTPEntities.put(pAdmin1.getID(), pAdmin1);
-		mTPEntities.put(pAdmin2.getID(), pAdmin2);
-
-		TreeParent root = new TreeParent("", TreeObject.FOLDER_TYPE,
-				TreeObject.FOLDER_IMAGE);
-		root.addChild(pProj);
-
-		return root;
-	}
-
 	/**
 	 * This is a callback that will allow us to create the viewer and initialize
 	 * it.
@@ -146,7 +91,7 @@ public class TestView extends ViewPart implements Observer {
 
 		Activator.getDefault().getEventAdminHandler().addObserver(this);
 
-		mTPEntities = new HashMap<String, TPTestEntity>();
+		mTPEntities = new HashMap<String, TPEntity>();
 
 		runTest = new Action("Run...") {
 			public void run() {
@@ -158,7 +103,7 @@ public class TestView extends ViewPart implements Observer {
 				| SWT.BORDER);
 		viewer.setContentProvider(new TestContentProvider());
 		viewer.setLabelProvider(new TestLabelProvider());
-		viewer.setInput(createDummyModel());
+		viewer.setInput(null);
 		getSite().setSelectionProvider(viewer);
 
 		createActions();
@@ -181,9 +126,11 @@ public class TestView extends ViewPart implements Observer {
 			if (/*
 				 * tpEntity != null &
 				 */tpEvent.getTopic().equals(ITPBridge.TEST_EXEC_RESULT_TOPIC)) {
+				
 				System.out.println("TestView: update called for "
 						+ tpEvent.getTopic() + " Event for "
 						+ tpEvent.getTestName());
+				/*
 				String status = tpEvent.getStatus();
 				String successPath = TreeObject.TEST_OK_IMAGE;
 				if (status.indexOf("pass") < 0) {
@@ -199,10 +146,21 @@ public class TestView extends ViewPart implements Observer {
 					}
 
 				});
+				*/
 			} else if (tpEvent.getTopic().equalsIgnoreCase(
 					ITPBridge.TEST_TREE_GET_RESP_TOPIC)) {
-				System.out.println(tpEvent.getDictionary().get(
-						TPEvent.TEST_TREE_XML_KEY));
+				String testTreeXML = tpEvent.getDictionary().get(
+						TPEvent.TEST_TREE_XML_KEY);
+				System.out.println(testTreeXML);
+				final Document dom = XMLUtil.getDocFromXml(testTreeXML);
+
+				Display.getDefault().syncExec(new Runnable() {
+					public void run() {
+						//viewer.refresh(TestXML.getTPEntityFromDoc(dom), true);
+						// viewer.update(tpEntity, null);
+						viewer.setInput(TestXML.getTPEntityFromDoc(dom));
+					}
+				});
 			}
 
 		}

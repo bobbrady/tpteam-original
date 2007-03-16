1ba700a24bc774d2bed6c9c8a9afbe415f36fbba
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-03-16 21:07:32 +0000

    Reorganized imports.

--- trunk/tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/eventadmin/EventAdminClient.java
+++ trunk/tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/eventadmin/EventAdminClient.java
@@ -16,7 +16,6 @@ import org.osgi.service.event.EventAdmin;
 import org.osgi.util.tracker.ServiceTracker;
 
 import edu.harvard.fas.rbrady.tpteam.tpbridge.bridge.ITPBridge;
-import edu.harvard.fas.rbrady.tpteam.tpbridge.model.TPEvent;
 
 public class EventAdminClient {
 

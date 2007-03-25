1ec5571d94033e2d59e99c259831b12929aa4898
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-03-25 19:16:32 +0000

    Added ErrorDialog w/Stacktrace details when ECF login fails.

--- trunk/tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/actions/URIClientConnectAction.java
+++ trunk/tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/actions/URIClientConnectAction.java
@@ -14,9 +14,12 @@ import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
 
 import edu.harvard.fas.rbrady.tpteam.tpbuddy.Activator;
 import edu.harvard.fas.rbrady.tpteam.tpbuddy.tpbridge.TPBridgeClient;
@@ -72,8 +75,19 @@ public class URIClientConnectAction implements IWorkbenchWindowActionDelegate {
             	TPBridgeClient client = Activator.getDefault().getTPBridgeClient();
                 client.createAndConnectClient(containerType, uri,nickname, data);
                 return status;
-            } catch (Exception e) {
-            	e.printStackTrace();
+            } catch (final Exception e) {
+				Display.getDefault().syncExec(new Runnable() {
+					public void run() {
+						String msg = "ECF Login Error: " + e.getMessage() + ". Please try again.";
+						MultiStatus info = new MultiStatus(Activator.PLUGIN_ID, 1, msg, null);
+						for(StackTraceElement trace : e.getStackTrace())
+						{
+							info.add(new Status(IStatus.ERROR, Activator.PLUGIN_ID, 1, trace.toString(), null));
+						}
+		            	ErrorDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "ECF Login Error", null, info);
+					}
+				});
+
             	showExceptionInMultiStatus(15555,status,e);
                 return status;
             }

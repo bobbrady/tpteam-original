43013e833b3b26becd0e849ce8ad362efeb23861
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-04-04 22:36:56 +0000

    Added skeleton functionality for Project Report View.

---
+++ tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/views/ReportView.java
@@ -0,0 +1,26 @@
+package edu.harvard.fas.rbrady.tpteam.tpbuddy.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ReportView extends ViewPart {
+	
+	public static final String ID = "edu.harvard.fas.rbrady.tpteam.tpbuddy.views.reportview";
+
+	public ReportView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

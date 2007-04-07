6ff3edca060b1b7d8526829e457fc5532149f4d5
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-04-07 18:51:10 +0000

    Made changes to sync Test Tree and Report views w/Project selection.

--- trunk/tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/charts/PieChart.java
+++ trunk/tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/charts/PieChart.java
@@ -1,10 +1,15 @@
 package edu.harvard.fas.rbrady.tpteam.tpbuddy.charts;
 
+import java.util.List;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.AbstractDataset;
 import org.jfree.data.general.DefaultPieDataset;
+
+import edu.harvard.fas.rbrady.tpteam.tpbridge.chart.ChartDataPoint;
+import edu.harvard.fas.rbrady.tpteam.tpbridge.chart.ChartDataSet;
 
 public class PieChart extends AbstractChart {
 
@@ -30,6 +35,20 @@ public class PieChart extends AbstractChart {
 		dataset.setValue(FAIL, 20);
 		dataset.setValue(ERR, 4);
 		dataset.setValue(INCONCL, 0);
+		dataset.setValue(NOTEXEC, 3);
+		return dataset;
+	}
+
+	protected AbstractDataset createDataset(ChartDataSet dataSet) {
+		List<ChartDataPoint> dataPoints = dataSet.getChartDataPoints();
+		ChartDataPoint dataPoint = dataPoints.get(0);
+
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		dataset.setValue(PASS, dataPoint.getPass());
+		dataset.setValue(FAIL, dataPoint.getFail());
+		dataset.setValue(ERR, dataPoint.getError());
+		dataset.setValue(INCONCL, dataPoint.getInconcl());
+		dataset.setValue(NOTEXEC, dataPoint.getNotExec());
 		return dataset;
 	}
 
@@ -47,7 +66,8 @@ public class PieChart extends AbstractChart {
 
 		// create the chart...
 		JFreeChart chart = ChartFactory.createPieChart(
-				"Project Test Execution Overview", dataset, true, // legend?
+				"Project Test Execution Overview\nTotal Tests = 5", dataset,
+				true, // legend?
 				true, // tooltips?
 				false // URLs?
 				);
@@ -58,6 +78,33 @@ public class PieChart extends AbstractChart {
 		plot.setSectionPaint(FAIL, RED);
 		plot.setSectionPaint(ERR, YELLOW);
 		plot.setSectionPaint(INCONCL, BLUE);
+		plot.setSectionPaint(NOTEXEC, PINK);
+
+		return chart;
+	}
+
+	public JFreeChart createChart(ChartDataSet dataSet) {
+
+		DefaultPieDataset dataset = (DefaultPieDataset) createDataset(dataSet);
+		int totalTests = 0;
+		for (int idx = 0; idx < dataset.getItemCount(); idx++)
+			totalTests += dataset.getValue(idx).intValue();
+
+		// create the chart...
+		JFreeChart chart = ChartFactory.createPieChart(dataSet.getProjName()
+				+ " Test Plan Overview\nTotal Tests: " + totalTests, dataset,
+				true, // legend?
+				true, // tooltips?
+				false // URLs?
+				);
+
+		// Set custom settings
+		PiePlot plot = (PiePlot) chart.getPlot();
+		plot.setSectionPaint(PASS, GREEN);
+		plot.setSectionPaint(FAIL, RED);
+		plot.setSectionPaint(ERR, YELLOW);
+		plot.setSectionPaint(INCONCL, BLUE);
+		plot.setSectionPaint(NOTEXEC, PINK);
 
 		return chart;
 	}

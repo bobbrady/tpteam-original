3dae3cc6b74c6001ce39c0167cc0bd62d1862c10
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-04-07 21:09:25 +0000

    Added features for Project User Bar Chart overiew.

--- trunk/tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/charts/BarChart.java
+++ trunk/tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/charts/BarChart.java
@@ -1,6 +1,7 @@
 package edu.harvard.fas.rbrady.tpteam.tpbuddy.charts;
 
 import java.awt.Color;
+import java.util.Arrays;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
@@ -13,6 +14,10 @@ import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.AbstractDataset;
+
+import edu.harvard.fas.rbrady.tpteam.tpbridge.chart.ChartDataPoint;
+import edu.harvard.fas.rbrady.tpteam.tpbridge.chart.ChartDataSet;
+import edu.harvard.fas.rbrady.tpteam.tpbridge.hibernate.TpteamUser;
 
 public class BarChart extends AbstractChart{
 	
@@ -74,7 +79,25 @@ public class BarChart extends AbstractChart{
       return dataset;
 
    }
+   
+   protected AbstractDataset createDataset(ChartDataSet[] dataSets) {
+	      DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	      Arrays.sort(dataSets);
+	      for(ChartDataSet tpTeamDataSet : dataSets)
+	      {
+	    	  TpteamUser user = tpTeamDataSet.getUser();
+	    	  String userName = user.getLastName() + ", " + user.getFirstName();
+	    	  ChartDataPoint tpTeamDataPoints = tpTeamDataSet.getChartDataPoints().get(0);
+	    	  dataset.addValue(tpTeamDataPoints.getPass(), PASS, userName);
+	    	  dataset.addValue(tpTeamDataPoints.getFail(), FAIL, userName);
+	    	  dataset.addValue(tpTeamDataPoints.getError(), ERR, userName);
+	    	  dataset.addValue(tpTeamDataPoints.getInconcl(), INCONCL, userName);
+	    	  dataset.addValue(tpTeamDataPoints.getNotExec(), NOTEXEC, userName);
+	      }
+	      return dataset;
+	   }
 
+   
    /**
     * Creates a sample chart.
     *
@@ -130,4 +153,53 @@ public class BarChart extends AbstractChart{
       return chart;
 
    }
+   
+   public JFreeChart createChart(ChartDataSet[] dataSets, String projName) {
+
+	      CategoryDataset dataset = (CategoryDataset)createDataset(dataSets);
+
+	      // create the chart...
+	      JFreeChart chart = ChartFactory.createBarChart("Project " + projName + " Users Overview", // title
+	            "User", // domain axis label
+	            "Number of Tests", // range axis label
+	            dataset, // data
+	            PlotOrientation.VERTICAL, // orientation
+	            true, // include legend
+	            true, // tooltips
+	            false // URLs
+	            );
+
+	      // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+	      // set the background color for the chart...
+	      chart.setBackgroundPaint(Color.white);
+
+	      // get a reference to the plot for further customisation...
+	      CategoryPlot plot = chart.getCategoryPlot();
+	      plot.setBackgroundPaint(Color.lightGray);
+	      plot.setDomainGridlinePaint(Color.white);
+	      plot.setDomainGridlinesVisible(true);
+	      plot.setRangeGridlinePaint(Color.white);
+
+	      // set the range axis to display integers only...
+	      final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+	      rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+	      // disable bar outlines...
+	      BarRenderer renderer = (BarRenderer) plot.getRenderer();
+	      renderer.setSeriesPaint(0, GREEN);
+	      renderer.setSeriesPaint(1, RED);
+	      renderer.setSeriesPaint(2, YELLOW);
+	      renderer.setSeriesPaint(3, BLUE);
+	      renderer.setSeriesPaint(4, PINK);
+	      renderer.setDrawBarOutline(false);
+
+	      CategoryAxis domainAxis = plot.getDomainAxis();
+	      domainAxis.setCategoryLabelPositions(CategoryLabelPositions
+	            .createUpRotationLabelPositions(Math.PI / 6.0));
+	      // OPTIONAL CUSTOMISATION COMPLETED.
+
+	      return chart;
+
+	   }
 }

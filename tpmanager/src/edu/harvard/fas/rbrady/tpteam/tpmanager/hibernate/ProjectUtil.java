ef1aff532712d7db9b234816be89a2c9331e4033
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-05-04 02:26:48 +0000

    Added TestProjectUtil to testplugin.

--- tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/hibernate/ProjectUtil.java
+++ tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/hibernate/ProjectUtil.java
@@ -8,6 +8,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import edu.harvard.fas.rbrady.tpteam.tpbridge.hibernate.HibernateUtil;
 import edu.harvard.fas.rbrady.tpteam.tpbridge.hibernate.Product;
 import edu.harvard.fas.rbrady.tpteam.tpbridge.hibernate.Project;
 import edu.harvard.fas.rbrady.tpteam.tpbridge.hibernate.TpteamUser;
@@ -16,25 +17,29 @@ import edu.harvard.fas.rbrady.tpteam.tpbridge.xml.ProjectXML;
 import edu.harvard.fas.rbrady.tpteam.tpmanager.Activator;
 
 public class ProjectUtil {
+
 	public static Set<Project> getProjByECFID(String ecfID) throws Exception {
 		Set<Project> projs = null;
 		Session s = null;
 		Transaction tx = null;
 		try {
-
-			s = Activator.getDefault().getHiberSessionFactory().getCurrentSession();
-
-			// s = HibernateUtil.getSessionFactory().getCurrentSession();
+			// If in OSGi envrionment, grab session from plugin activator
+			if (Activator.getDefault() != null) {
+				s = Activator.getDefault().getHiberSessionFactory()
+						.getCurrentSession();
+			} else {
+				s = HibernateUtil.getSessionFactory().getCurrentSession();
+			}
 			tx = s.beginTransaction();
 
 			String hql = "from TpteamUser as user where user.ecfId =:ecfID";
 			Query query = s.createQuery(hql);
 			query.setString("ecfID", ecfID);
 			TpteamUser user = (TpteamUser) query.uniqueResult();
-			if(user == null)
+			if (user == null)
 				return new HashSet<Project>();
 			projs = user.getProjects();
-			if(projs == null)
+			if (projs == null)
 				return new HashSet<Project>();
 			for (Project proj : projs) {
 				proj.initSkeleton();
@@ -51,42 +56,38 @@ public class ProjectUtil {
 	public static String getProjProdXML(TPEvent tpEvent) throws Exception {
 		Set<Project> projs = getProjByECFID(tpEvent.getDictionary().get(
 				TPEvent.FROM));
-		/****************************************************************
+		/***********************************************************************
 		 * Create stub Projects
 		 * 
 		 * Betwixt will call all getter methods objects as part of its
-		 * introspection.  This will cause non-initialized objects loaded
-		 * by Hibernate to throw init errors outside of Hibernate session.
-		 * Stub objects allow us to use lazy loading and avoid this error. 
-		 ******************************************************************/
+		 * introspection. This will cause non-initialized objects loaded by
+		 * Hibernate to throw init errors outside of Hibernate session. Stub
+		 * objects allow us to use lazy loading and avoid this error.
+		 **********************************************************************/
 		HashSet<Project> stubProjs = new HashSet<Project>();
-		for(Project proj : projs)
-		{
-			Product stubProd = new Product(proj.getProduct().getId(), proj.getProduct().getName(), null, null);
-			Project stubProj = new Project(proj.getId(), stubProd, proj.getName(), proj.getDescription(), null, null);
+		for (Project proj : projs) {
+			Product stubProd = new Product(proj.getProduct().getId(), proj
+					.getProduct().getName(), null, null);
+			Project stubProj = new Project(proj.getId(), stubProd, proj
+					.getName(), proj.getDescription(), null, null);
 			stubProjs.add(stubProj);
 		}
 		return ProjectXML.getXML(stubProjs);
 	}
-	
 
 	public static void main(String[] args) {
 		Set<Project> projs;
 		try {
-			projs = getProjByECFID("tpteam_2@jabber.org");
+			projs = getProjByECFID("tpteam1@gmail.com");
 			for (Project proj : projs) {
 				System.out.println("Proj ID: " + proj.getId() + ", Name: "
 						+ proj.getName() + ", Desc: " + proj.getDescription());
 				System.out.println("Prod ID: " + proj.getProduct().getId()
 						+ ", Name: " + proj.getProduct().getName());
-
-				for (TpteamUser user : proj.getTpteamUsers()) {
-					System.out.println("TpteamUser ECFID: " + user.getEcfId());
-				}
 			}
 
 			Hashtable<String, String> hash = new Hashtable<String, String>();
-			hash.put(TPEvent.FROM, "tpteam_2@jabber.org");
+			hash.put(TPEvent.FROM, "tpteam1@gmail.com");
 			TPEvent tpEvent = new TPEvent("topic", hash);
 			String xml = getProjProdXML(tpEvent);
 			System.out.println("ProjProdXML: \n" + xml);

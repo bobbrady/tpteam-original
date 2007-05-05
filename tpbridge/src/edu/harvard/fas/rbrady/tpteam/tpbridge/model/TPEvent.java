e312e227788b57728a25309cec5d4a6e760746e5
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-05-05 19:52:16 +0000

    Cleaned up.

--- tpbridge/src/edu/harvard/fas/rbrady/tpteam/tpbridge/model/TPEvent.java
+++ tpbridge/src/edu/harvard/fas/rbrady/tpteam/tpbridge/model/TPEvent.java
@@ -13,141 +13,112 @@ import java.util.Hashtable;
 
 import org.osgi.service.event.Event;
 
-public class TPEvent implements Serializable{
+public class TPEvent implements Serializable {
 
 	private static final long serialVersionUID = 7318549536622346381L;
 
 	public static final String PROJECT_KEY = "PROJECT";
-	
+
 	public static final String PROJECT_ID_KEY = "PROJ_ID";
-	
+
 	public static final String PROJ_PROD_XML_KEY = "PROD_PROJ_XML";
-	
+
 	public static final String TEST_XML_KEY = "TEST_XML";
-	
+
 	public static final String TEST_TREE_XML_KEY = "TEST_TREE_XML";
-	
+
 	public static final String TEST_EXEC_XML_KEY = "TEST_EXEC_XML";
-	
+
 	public static final String TEST_PROP_XML_KEY = "TEST_PROP_XML";
-	
+
 	public static final String CHART_DATASET_XML_KEY = "CHART_DATASET_XML_KEY";
 
 	public static final String PARENT_CONTAINER_KEY = "PARENT_CONTAINER";
-	
+
 	public static final String VERDICT_KEY = "VERDICT";
 
 	public static final String TEST_NAME_KEY = "TEST_NAME";
-	
+
 	public static final String TEST_DESC_KEY = "TEST_DESC";
-	
+
 	public static final String TIMESTAMP_KEY = "TIMESTAMP";
-	
+
 	public static final String COMMENTS_KEY = "COMMENTS";
 
 	public static final String ID_KEY = "ID";
-	
+
 	public static final String PARENT_ID_KEY = "PARENT_ID";
-	
+
 	public static final String ECFID_KEY = "ECFID";
 
 	public static final String STATUS_KEY = "STATUS";
-	
+
 	public static final String SEND_TO = "SEND_TO";
-	
+
 	public static final String FROM = "FROM";
-	
+
 	private Hashtable<String, String> mDictionary;
 
 	private String mTopic;
-	
+
 	public TPEvent(Event event) {
 		mDictionary = new Hashtable<String, String>();
-		/*
-		mDictionary.put(PROJECT_KEY, (String)event.getProperty(PROJECT_KEY));
-		mDictionary.put(PARENT_CONTAINER_KEY,(String)event.getProperty(PARENT_CONTAINER_KEY));
-		mDictionary.put(TEST_NAME_KEY,(String)event.getProperty(TEST_NAME_KEY));
-		mDictionary.put(ID_KEY,(String)event.getProperty(ID_KEY));
-		mDictionary.put(STATUS_KEY,(String)event.getProperty(STATUS_KEY));
-		mDictionary.put(SEND_TO,(String)event.getProperty(SEND_TO));
-		String ecfContainerID = (String)event.getProperty(ITPBridge.CONTAINER_ID_KEY);
-		String ecfSharedObjectID = (String)event.getProperty(ITPBridge.SHARED_OBJECT_ID_KEY);
-		
-		mDictionary.put(ITPBridge.CONTAINER_ID_KEY, ecfContainerID);
-		mDictionary.put(ITPBridge.SHARED_OBJECT_ID_KEY, ecfSharedObjectID);
-		*/
-		for(String propName : event.getPropertyNames())
-			mDictionary.put(propName, (String)event.getProperty(propName));
-		
+		for (String propName : event.getPropertyNames())
+			mDictionary.put(propName, (String) event.getProperty(propName));
+
 		mTopic = event.getTopic();
 	}
 
-	public TPEvent(String topic, String project, String parent, String name, String id, String status) {
+	public TPEvent(String topic, String project, String parent, String name,
+			String id, String status) {
 		mDictionary = new Hashtable<String, String>();
 		mDictionary.put(PROJECT_KEY, project);
-		mDictionary.put(PARENT_CONTAINER_KEY,parent);
-		mDictionary.put(TEST_NAME_KEY,name);
-		mDictionary.put(ID_KEY,id);
-		mDictionary.put(STATUS_KEY,status);
+		mDictionary.put(PARENT_CONTAINER_KEY, parent);
+		mDictionary.put(TEST_NAME_KEY, name);
+		mDictionary.put(ID_KEY, id);
+		mDictionary.put(STATUS_KEY, status);
 		mTopic = topic;
 	}
 
-	public TPEvent(String topic, Hashtable<String,String> dictionary) {
-		
+	public TPEvent(String topic, Hashtable<String, String> dictionary) {
+
 		mDictionary = dictionary;
 		mTopic = topic;
 	}
 
-	
-	public static TPEvent[] getExamples() {
-		TPEvent[] tpEvents = new TPEvent[2];
-		tpEvents[0] = new TPEvent("topic1", "project1", "parent1", "test name1", "1", "pass");
-		tpEvents[1] = new TPEvent("topic1", "project2", "parent2", "test name2", "2", "fail");
-		return tpEvents;
-	}
-	
-	public void setTopic(String topic)
-	{
+	public void setTopic(String topic) {
 		mTopic = topic;
 	}
-	
-	public String getTopic()
-	{
+
+	public String getTopic() {
 		return mTopic;
 	}
 
-	public Hashtable<String,String> getDictionary()
-	{
+	public Hashtable<String, String> getDictionary() {
 		return mDictionary;
 	}
-	
-	public String getProject()
-	{
-		return (String)mDictionary.get(PROJECT_KEY);
+
+	public String getProject() {
+		return (String) mDictionary.get(PROJECT_KEY);
 	}
-	
-	public String getParentContainer()
-	{
-		return (String)mDictionary.get(PARENT_CONTAINER_KEY);
+
+	public String getParentContainer() {
+		return (String) mDictionary.get(PARENT_CONTAINER_KEY);
 	}
-	
-	public String getTestName()
-	{
-		return (String)mDictionary.get(TEST_NAME_KEY);
+
+	public String getTestName() {
+		return (String) mDictionary.get(TEST_NAME_KEY);
 	}
-	
-	public String getID()
-	{
-		return (String)mDictionary.get(ID_KEY);
+
+	public String getID() {
+		return (String) mDictionary.get(ID_KEY);
 	}
-	
-	public String getStatus()
-	{
-		return (String)mDictionary.get(STATUS_KEY);
+
+	public String getStatus() {
+		return (String) mDictionary.get(STATUS_KEY);
 	}
-	
-	public void setStatus(String status)
-	{
+
+	public void setStatus(String status) {
 		mDictionary.put(STATUS_KEY, status);
 	}
 

699122f5464f16f7cdc9f1dd55d1b63d398c3819
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-05-08 01:16:35 +0000

    Refactored multi-threaded methods into two separate classes for CRUD & Execution.

--- tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/http/admin/exec/AdminProcessTestExec.java
+++ tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/http/admin/exec/AdminProcessTestExec.java
@@ -10,6 +10,7 @@
 package edu.harvard.fas.rbrady.tpteam.tpmanager.http.admin.exec;
 
 import java.io.IOException;
+import java.util.Hashtable;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -20,15 +21,11 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import edu.harvard.fas.rbrady.tpteam.tpbridge.bridge.ITPBridge;
-import edu.harvard.fas.rbrady.tpteam.tpbridge.hibernate.Project;
 import edu.harvard.fas.rbrady.tpteam.tpbridge.hibernate.Test;
-import edu.harvard.fas.rbrady.tpteam.tpbridge.hibernate.TpteamUser;
-import edu.harvard.fas.rbrady.tpteam.tpbridge.model.TPEntity;
 import edu.harvard.fas.rbrady.tpteam.tpbridge.model.TPEvent;
-import edu.harvard.fas.rbrady.tpteam.tpbridge.xml.TestExecutionXML;
 import edu.harvard.fas.rbrady.tpteam.tpmanager.Activator;
-import edu.harvard.fas.rbrady.tpteam.tpmanager.hibernate.TestExecutionUtil;
 import edu.harvard.fas.rbrady.tpteam.tpmanager.http.ServletUtil;
+import edu.harvard.fas.rbrady.tpteam.tpmanager.tptp.TPTestExec;
 
 public class AdminProcessTestExec extends ServletUtil {
 
@@ -37,7 +34,7 @@ public class AdminProcessTestExec extends ServletUtil {
 	protected String mRemoteUser;
 
 	protected String mTestID;
-	
+
 	protected TPEvent mTPEvent;
 
 	public void init(ServletConfig config) throws ServletException {
@@ -50,87 +47,40 @@ public class AdminProcessTestExec extends ServletUtil {
 		mRemoteUser = req.getRemoteUser();
 		mTestID = req.getParameter("testID");
 
-		try 
-		{
+		try {
 			validateTestReq(req);
 			StringBuffer reply = new StringBuffer(execTest());
 			showPage(req, resp, reply, null, this);
 		} catch (Exception e) {
-			StringBuffer error = new StringBuffer("<h3>Error: " + e.getMessage() + "<br>"
-					+ e.getCause() + "</h3>");
+			StringBuffer error = new StringBuffer("<h3>Error: "
+					+ e.getMessage() + "<br>" + e.getCause() + "</h3>");
 			throwError(req, resp, error, this);
 			return;
 		}
 	}
-	
-	protected String execTest() throws Exception
-	{
+
+	protected String execTest() throws Exception {
+
+		Hashtable<String, String> dictionary = new Hashtable<String, String>();
+		String fromECFID = Activator.getDefault().getTPBridgeClient()
+				.getTPMgrECFID();
+		dictionary.put(TPEvent.FROM, fromECFID);
+		mTPEvent = new TPEvent(ITPBridge.TEST_EXEC_REQ_TOPIC, dictionary);
+
+		TPTestExec.runTest(mTestID, mTPEvent);
+		TPTestExec.sendTestExecResponse(mTPEvent);
+
 		StringBuffer reply = new StringBuffer();
-		Transaction tx = null;
-		String testType = null;
-	
-		try {
-			// For standalone
-			// Session s =
-			// HibernateUtil.getSessionFactory().getCurrentSession();
+		reply
+				.append("<h3 align=\"center\">TPManager Test Execution Result Page</h3>");
+		reply.append("<h3>Test Name: " + mTPEvent.getTestName() + "</h3>");
+		reply.append("<h3>Exec Username: " + mRemoteUser
+				+ "</h3>\n<h3>testID: " + mTestID + "</h3>");
+		reply.append("<h3>TestSuite Execution Verdict: "
+				+ mTPEvent.getDictionary().get(TPEvent.VERDICT_KEY) + "</h3>");
 
-			Session s = Activator.getDefault().getHiberSessionFactory()
-					.getCurrentSession();
-			tx = s.beginTransaction();
-			
-			
-			Test test = (Test) s.load(Test.class, new Integer(mTestID));
-			
-			testType = test.getTestType().getName();
-				
-			mTPEvent = new TPEvent(ITPBridge.TEST_EXEC_RESULT_TOPIC,
-					"Demo Project (TPTeam)", mRemoteUser, test.getName(),
-					mTestID, "status");
-
-			// Add all project user's ECF IDs to SEND_TO field of event
-			Project proj = (Project)s.load(Project.class, new Integer(test.getProject().getId()));
-			StringBuffer userECF = new StringBuffer();
-			for(TpteamUser user : proj.getTpteamUsers())
-			{
-				if(userECF.length() == 0)
-					userECF.append(user.getEcfId());
-				else
-					userECF.append("/" + user.getEcfId());
-			}
-			mTPEvent.getDictionary().put(TPEvent.SEND_TO, userECF.toString());
-			String ECFID = Activator.getDefault().getTPBridgeClient().getTPMgrECFID();
-			mTPEvent.getDictionary().put(TPEvent.ECFID_KEY, ECFID);
-			mTPEvent.getDictionary().put(TPEvent.TEST_NAME_KEY, test.getName());
-
-			
-			s.flush();
-			tx.commit();			
-			
-			if(testType.equalsIgnoreCase("JUNIT"))
-			{
-				Activator.getDefault().getTPManager().runJUnitTest(mTestID, mTPEvent);
-			}
-			TestExecutionUtil.insertTestExec(mTestID, mTPEvent);
-			sendTestExecResponse(mTPEvent);
-
-			reply
-					.append("<h3 align=\"center\">TPManager Test Execution Result Page</h3>");
-			reply.append("<h3>Test Name: " + mTPEvent.getTestName()
-					+ "</h3>");
-			reply.append("<h3>Exec Username: " + mRemoteUser + "</h3>\n<h3>testID: "
-					+ mTestID + "</h3>");
-			reply.append("<h3>TestSuite Execution Verdict: "
-					+ mTPEvent.getDictionary().get(TPEvent.VERDICT_KEY) + "</h3>");
-			
-
-		} catch (Exception e) {
-			if (tx != null)
-				tx.rollback();
-			throw e;
-		}
 		return reply.toString();
 	}
-
 
 	protected void validateTestReq(HttpServletRequest req) throws Exception {
 		/*
@@ -142,15 +92,18 @@ public class AdminProcessTestExec extends ServletUtil {
 			// For standalone
 			// Session s =
 			// HibernateUtil.getSessionFactory().getCurrentSession();
-			
+
 			Session s = Activator.getDefault().getHiberSessionFactory()
 					.getCurrentSession();
 			tx = s.beginTransaction();
 			Test test = (Test) s.load(Test.class, new Integer(mTestID));
 
-			if(Integer.parseInt(mTestID) <= 0 || test.getIsFolder() == 'Y' || test.getIsFolder() == 'y')
-			{
-				throw new Exception("Invalid Test Definition Selected for Execution", new Throwable("A Test Folders was Selected for Execution"));
+			if (Integer.parseInt(mTestID) <= 0 || test.getIsFolder() == 'Y'
+					|| test.getIsFolder() == 'y') {
+				throw new Exception(
+						"Invalid Test Definition Selected for Execution",
+						new Throwable(
+								"A Test Folders was Selected for Execution"));
 			}
 
 			s.flush();
@@ -160,23 +113,5 @@ public class AdminProcessTestExec extends ServletUtil {
 				tx.rollback();
 			throw e;
 		}
-	}
-	
-	private void sendTestExecResponse(TPEvent tpEvent) {
-		tpEvent.setTopic(ITPBridge.TEST_EXEC_RESULT_TOPIC);
-		tpEvent.getDictionary().put(TPEvent.FROM,
-				Activator.getDefault().getTPBridgeClient().getTPMgrECFID());
-
-		TPEntity execEntity = TestExecutionXML
-				.getTPEntityFromExecEvent(tpEvent);
-		String execXML = TestExecutionXML.getTPEntityXML(execEntity);
-		tpEvent.getDictionary().put(TPEvent.TEST_EXEC_XML_KEY, execXML);
-
-		System.out.println("TPManager runTests(): sending tpEvent w/verdict "
-				+ tpEvent.getDictionary().get(TPEvent.VERDICT_KEY) + " & topic " + tpEvent.getTopic()
-				+ " to " + tpEvent.getDictionary().get(TPEvent.SEND_TO));
-
-		Activator.getDefault().getEventAdminClient().sendEvent(
-				tpEvent.getTopic(), tpEvent.getDictionary());
 	}
 }

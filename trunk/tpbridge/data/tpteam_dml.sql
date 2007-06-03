3554ab61aea4435ffb182f8eb0641d7dff677c01
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-06-03 00:29:59 +0000

    Added comments to ddl. Initial commit of dml.

---
+++ trunk/tpbridge/data/tpteam_dml.sql
@@ -0,0 +1,28 @@
+/***************************************************************
+* File: tpteam_dml.sql
+* Description:  DML statements for inital TPTeam seed data
+* Author: Bob Brady, rpbrady@gmail.com
+* Date: 05Jun2007
+****************************************************************/
+
+/* Initial Roles, used by web interface */
+
+INSERT INTO ROLE (ROLE_ID, NAME, DESCRIPTION) 
+	VALUES (1, 'admin', 'Admin role');
+
+INSERT INTO ROLE (ROLE_ID, NAME, DESCRIPTION) 
+	VALUES (2, 'user', 'Basic User role');
+
+/* Initial administrative user, for web interface 
+   Note: Passwords are SHA1 hashes */
+
+INSERT INTO TPTEAM_USER (ID, USER_NAME, PASSWORD, ROLE_ID) 
+	VALUES (1, 'tpteam1',  '84FE0D304EE0B3D09C860FA4544E17C5D94D29E0',1);
+
+/* Mandatory test types used by TPTeam */
+
+INSERT INTO TEST_TYPE (ID, NAME)
+	VALUES (1, 'Folder');
+
+INSERT INTO TEST_TYPE (ID, NAME)
+	VALUES (2, 'JUnit');

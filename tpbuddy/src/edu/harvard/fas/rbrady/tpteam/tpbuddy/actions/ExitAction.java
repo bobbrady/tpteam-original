896eeb0fb1c125f81e5d352ac1bf5a4dc0b72dd7
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-06-09 18:26:54 +0000

    Added exit action to top-level menu.

---
+++ tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/actions/ExitAction.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Ed Burnette, Composent, Inc. and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Ed Burnette, Composent, Inc. - initial API and implementation
+ ******************************************************************************/
+package edu.harvard.fas.rbrady.tpteam.tpbuddy.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.ActionDelegate;
+import org.eclipse.ui.actions.ActionFactory;
+
+public class ExitAction extends ActionDelegate implements
+		IWorkbenchWindowActionDelegate {
+	
+	private IWorkbenchWindow window;
+
+	public void run() {
+	}
+
+	protected IWorkbench getWorkbench() {
+		return PlatformUI.getWorkbench();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#dispose()
+	 */
+	public void dispose() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
+	 */
+	public void init(IWorkbenchWindow window) {
+		this.window = window;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	public void run(IAction action) {
+		ActionFactory.QUIT.create(window).run();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction,
+	 *      org.eclipse.jface.viewers.ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+}

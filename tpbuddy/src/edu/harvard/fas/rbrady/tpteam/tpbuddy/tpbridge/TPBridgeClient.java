674f6b003edb354d1588c6081fa5d57e75507104
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-06-16 22:01:48 +0000

    Added JavaDoc comments.

--- tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/tpbridge/TPBridgeClient.java
+++ tpbuddy/src/edu/harvard/fas/rbrady/tpteam/tpbuddy/tpbridge/TPBridgeClient.java
@@ -1,11 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2005 Ed Burnette, Composent, Inc. and others. All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+/********************************************************************
  * 
- * Contributors: Ed Burnette, Composent, Inc. - initial API and implementation
- ******************************************************************************/
+ * File		:	TPBridgeClient.java
+ *
+ * Author	:	Bob Brady, rpbrady@gmail.com
+ * 
+ * Contents	:	Convenience client for the TPBridge OSGi Service
+ * 
+ ********************************************************************/
+
 package edu.harvard.fas.rbrady.tpteam.tpbuddy.tpbridge;
 
 import java.util.Hashtable;
@@ -13,7 +15,6 @@ import java.util.Hashtable;
 import org.eclipse.ecf.core.IContainer;
 import org.eclipse.ecf.core.identity.ID;
 import org.eclipse.ecf.core.security.IConnectContext;
-import org.eclipse.ecf.presence.ui.PresenceUI;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.PlatformUI;
 import org.osgi.framework.BundleContext;
@@ -23,9 +24,23 @@ import edu.harvard.fas.rbrady.tpteam.tpbridge.bridge.ITPBridge;
 import edu.harvard.fas.rbrady.tpteam.tpbridge.model.TPEvent;
 import edu.harvard.fas.rbrady.tpteam.tpbuddy.Activator;
 
+/*******************************************************************************
+ * File 		: 	TPBridgeClient.java
+ * 
+ * Description 	: 	Convenience client for the TPBridge OSGi Service
+ * 
+ * @author Bob Brady, rpbrady@gmail.com
+ * @version $Revision$
+ * @date $Date$ Copyright (c) 2007 Bob Brady
+ ******************************************************************************/
 public class TPBridgeClient extends Client {
-	PresenceUI presenceContainerUI = null;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param context
+	 *            the TPBuddy plug-in context
+	 */
 	public TPBridgeClient(BundleContext context) {
 		super(context);
 		setTPMgrECFID(getTPTeamProps().getProperty(TPMANAGER_ECFID_KEY));
@@ -37,8 +52,11 @@ public class TPBridgeClient extends Client {
 	 * TPManager
 	 * 
 	 * @param container
+	 *            the ECF XMPPS Container
 	 * @param targetID
+	 *            the ECF ID of the client
 	 * @param connectContext
+	 *            the ECF connection context
 	 */
 	public void connect(IContainer container, ID targetID,
 			IConnectContext connectContext) {
@@ -59,6 +77,7 @@ public class TPBridgeClient extends Client {
 	 * Sends a project request message to TPManager
 	 * 
 	 * @param ecfID
+	 *            the ECF ID of the intended recipient
 	 */
 	private void sendProjGetRequest(String ecfID) {
 		Hashtable<String, String> dictionary = new Hashtable<String, String>();

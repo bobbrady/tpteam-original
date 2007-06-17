8d70e497e8666e3b7ac17b9d4d0c037043e25d02
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-06-17 00:05:22 +0000

    Added JavaDoc comments.

--- trunk/tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/eventadmin/EventAdminClient.java
+++ trunk/tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/eventadmin/EventAdminClient.java
@@ -1,30 +1,53 @@
-/*******************************************************************************
- * Copyright (c) 2006 Robert Brady. All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+/********************************************************************
+ * 
+ * File		:	EventAdminClient.java
  *
- * Contributors: Robert Brady - initial API and implementation
- ******************************************************************************/
+ * Author	:	Bob Brady, rpbrady@gmail.com
+ * 
+ * Contents	:	A client of the EventAdmin Service 
+ *  
+ ********************************************************************/
 package edu.harvard.fas.rbrady.tpteam.tpmanager.eventadmin;
 
 import java.util.Hashtable;
-
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventAdmin;
 import org.osgi.util.tracker.ServiceTracker;
 
+/*******************************************************************************
+ * File 		: 	EventAdminClient.java
+ * 
+ * Description 	: 	A client of the EventAdmin Service.  It sends TPTeam Event
+ * 					data encapsulated as an OSGi Event
+ * 
+ * @author Bob Brady, rpbrady@gmail.com
+ * @version $Revision$
+ * @date $Date$ Copyright (c) 2007 Bob Brady
+ ******************************************************************************/
 public class EventAdminClient {
 
+	/** The EventAdmin ServiceTracker */
 	private ServiceTracker mServiceTracker;
 
+	/**
+	 * Constructor, gets and opens a tracker for
+	 * the EventAdmin OSGi Service
+	 * 
+	 * @param context The TPManager plug-in context
+	 */
 	public EventAdminClient(BundleContext context) {
 		mServiceTracker = new ServiceTracker(context, EventAdmin.class
 				.getName(), null);
 		mServiceTracker.open();
 	}
 
+	/**
+	 * Sends an OSGi Event to the OSGi EventAdmin Service
+	 * @param topic the event topic String
+	 * @param dictionary a table of key=value pairs
+	 * @return true if message sent successfully, false otherwise
+	 */
 	public boolean sendEvent(String topic, Hashtable<String, String> dictionary) {
 		boolean messageSent = false;
 

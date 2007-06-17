078cd9091917d0497c69c0b511957ced3cacda3b
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-06-17 00:05:22 +0000

    Added JavaDoc comments.

--- tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/eventadmin/EventAdminHandler.java
+++ tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/eventadmin/EventAdminHandler.java
@@ -1,31 +1,53 @@
-/*******************************************************************************
- * Copyright (c) 2006 Robert Brady. All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+/********************************************************************
+ * 
+ * File		:	EventAdminHandler.java
  *
- * Contributors: Robert Brady - initial API and implementation
- ******************************************************************************/
+ * Author	:	Bob Brady, rpbrady@gmail.com
+ * 
+ * Contents	:	The OSGi Service that handles OSGi Events from the 
+ * 				EventAdmin Service and acts as an observable of TPTeam 
+ * 				Events.  Notifies the TPManager object when events are 
+ * 				received. 
+ *   
+ ********************************************************************/
 package edu.harvard.fas.rbrady.tpteam.tpmanager.eventadmin;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Observable;
-
 import edu.harvard.fas.rbrady.tpteam.tpbridge.bridge.ITPBridge;
 import edu.harvard.fas.rbrady.tpteam.tpbridge.model.TPEvent;
-
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
 
+/*******************************************************************************
+ * File 		: 	EventAdminClient.java
+ * 
+ * Description 	: 	The OSGi Service that handles OSGi Events from the 
+ * 					EventAdmin Service and acts as an observable of TPTeam 
+ * 					Events.  Notifies the TPManager object when events are 
+ * 					received.
+ * 
+ * @author Bob Brady, rpbrady@gmail.com
+ * @version $Revision$
+ * @date $Date$ Copyright (c) 2007 Bob Brady
+ ******************************************************************************/
 public class EventAdminHandler extends Observable implements EventHandler {
-
+	/** Table of key=value pairs of the OSGi Event */
 	private Hashtable<String, String[]> mDictionary = new Hashtable<String, String[]>();
-
+	/** A List log of all TPEvents received */
 	private ArrayList<Event> mEvents;
 
+	/**
+	 * Constructor
+	 * 
+	 * Sets the OSGi Event topics to be handled by this service
+	 * 
+	 * Sets the 
+	 * @param context
+	 */
 	public EventAdminHandler(BundleContext context) {
 		mEvents = new ArrayList<Event>();
 		mDictionary.put(EventConstants.EVENT_TOPIC, new String[] {
@@ -41,6 +63,13 @@ public class EventAdminHandler extends Observable implements EventHandler {
 				.println("TPManager EventAdminHandler: registered EventAdmin service");
 	}
 
+	/**
+	 * Handles an OSGi Event from the EventAdmin Service
+	 * and notifies the TPManager so that it can
+	 * perform the necessary CRUD or execution operation.
+	 * 
+	 * @param event the OSGi Event to be handled
+	 */
 	public void handleEvent(Event event) {
 		System.out.println("TPManager EventAdminHandler: Got "
 				+ event.getTopic() + " Event");
@@ -49,8 +78,11 @@ public class EventAdminHandler extends Observable implements EventHandler {
 		notifyObservers(tpEvent);
 	}
 
+	/**
+	 * Getter
+	 * @return the List log of TPTeam Events
+	 */
 	public ArrayList<Event> getEventLog() {
 		return mEvents;
 	}
-
 }

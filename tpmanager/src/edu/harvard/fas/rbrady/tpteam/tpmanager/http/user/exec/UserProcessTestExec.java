35fd124f5178d4c51e54f1bb884f1aa901575d87
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-06-19 01:02:06 +0000

    Added JavaDoc comments.

--- tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/http/user/exec/UserProcessTestExec.java
+++ tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/http/user/exec/UserProcessTestExec.java
@@ -1,12 +1,14 @@
-/*******************************************************************************
- * Copyright (c) 2006 Robert Brady. All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+/********************************************************************
+ * 
+ * File		:	UserProcessTestExec.java
  *
- * Contributors: Robert Brady - initial API and implementation
- ******************************************************************************/
-
+ * Author	:	Bob Brady, rpbrady@gmail.com
+ * 
+ * Contents	:	Servlet that validates a test request
+ * 				and delegates its execution, for use by a
+ * 				non-administrative Web user
+ *  
+ ********************************************************************/
 package edu.harvard.fas.rbrady.tpteam.tpmanager.http.user.exec;
 
 import edu.harvard.fas.rbrady.tpteam.tpmanager.http.UserServlet;

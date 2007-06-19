e5e62584cb2f2f213170084dadd653a8be5a65c8
rpbrady <rpbrady@be2c06bc-6927-0410-8793-2b7aba0fa2bd>
2007-06-19 00:58:38 +0000

    Added JavaDoc comments.

--- trunk/tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/http/user/delete/DeleteTestEntityByUser.java
+++ trunk/tpmanager/src/edu/harvard/fas/rbrady/tpteam/tpmanager/http/user/delete/DeleteTestEntityByUser.java
@@ -1,12 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2006 Robert Brady. All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+/********************************************************************
+ * 
+ * File		:	DeleteTestEntityByUser.java
  *
- * Contributors: Robert Brady - initial API and implementation
- ******************************************************************************/
-
+ * Author	:	Bob Brady, rpbrady@gmail.com
+ * 
+ * Contents	:	Servlet that deletes a Test from the TPTeam 
+ * 				database, for use by a non-administrative Web user
+ *  
+ ********************************************************************/
 package edu.harvard.fas.rbrady.tpteam.tpmanager.http.user.delete;
 
 import edu.harvard.fas.rbrady.tpteam.tpmanager.http.UserServlet;
